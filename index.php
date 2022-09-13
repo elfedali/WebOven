f0e99cb62cd08a0321b8bda47bc1305fd28963f8
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2022-09-13 17:57:30 +0100

    template structure

---
+++ index.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebOven</title>
+    <!-- link css -->
+    <link rel="stylesheet" href="css/theme.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-white">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Web Oven</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- section -->
+
+    <section id="hero">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="hero-inner">
+                        <p>Our Goal</p>
+                        <h1>
+                            We help our clients realize their most important
+                            business goals
+                        </h1>
+
+                        <a href="#" class="btn btn-primary">Get Started</a>
+                        <!-- watch video -->
+                        <a href="#" class="btn btn-outline-primary">Watch Video</a>
+
+                    </div>
+                    <!-- /.hero-inner -->
+                </div>
+                <!-- /.col-lg-6 -->
+                <div class="col-lg-6">
+
+                    <img class="img-fluid" src="https://via.placeholder.com/600" alt="" srcset="">
+
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#hero -->
+
+
+    <section id="solution">
+        <div class="container">
+            <row>
+                <div class="col-lg-6">
+                    <h2>Custom IT solution<br>
+                        for your business</h2>
+                    <p>
+                        We are a group of talented engineer will experienced in our field
+                        We do build best techo fot our client.
+                    </p>
+                    <p>
+                        We are a group of talented engineer will experienced in our field
+                        We do build best techo fot our client. We are designer and developers
+                        writers illustrators, we put our expertise in our need, contact us for an eventuel
+                        or contact us by clicking bellow
+                    </p>
+                    <a href="#" class="btn btn-primary">Contact Us</a>
+                </div>
+                <!-- /.col-lg-6 -->
+
+                <div class="col-lg-6">
+                    <img src="https://via.placeholder.com/600" alt="" srcset="">
+                </div>
+            </row>
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#solution -->
+
+
+    <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+
+                    <h2>We help with</h2>
+                    <h3>Building better <br> solutions</h3>
+                    <!-- link -->
+                    <a href="#" class="btn btn-primary">Our Services</a>
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-lg-8">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <!-- bootstrap icon -->
+                                    <i class="bi bi-people-fill"></i>
+                                    <h5 class="card-title">Web Development</h5>
+                                    <p class="card-text">Naturally, study of cross-platform practices ensures integrity
+                                    </p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <!-- bootstrap icon -->
+                                    <i class="bi bi-people-fill"></i>
+                                    <h5 class="card-title">Web Development</h5>
+                                    <p class="card-text">Naturally, study of cross-platform practices ensures integrity
+                                    </p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <!-- bootstrap icon -->
+                                    <i class="bi bi-people-fill"></i>
+                                    <h5 class="card-title">Web Development</h5>
+                                    <p class="card-text">Naturally, study of cross-platform practices ensures integrity
+                                    </p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <!-- bootstrap icon -->
+                                    <i class="bi bi-people-fill"></i>
+                                    <h5 class="card-title">Web Development</h5>
+                                    <p class="card-text">Naturally, study of cross-platform practices ensures integrity
+                                    </p>
+                                    <a href="#" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.col-lg-8 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#about -->
+
+
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <img class="img-fluid" src="https://via.placeholder.com/600" alt="" srcset="">
+                </div>
+                <!-- /.col-6 -->
+                <div class="col-lg-6  py-5">
+                    <h2 class="text-light">Our Services</h2>
+                    <h3 class="text-light">What we are doing?</h3>
+                    <p class="text-light">
+                        We are a group of talented engineer will experienced in our field
+                        We do build best techo fot our client.
+                    </p>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3>Sed fringilla mauris sit</h3>
+                            <p>As a matter of fact the point of the criterion reveals the patterns of The Basis of Deep
+                                Estimation</p>
+
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <h3>Sed fringilla mauris sit</h3>
+                            <p>As a matter of fact the point of the criterion reveals the patterns of The Basis of Deep
+                                Estimation</p>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <h3>Sed fringilla mauris sit</h3>
+                            <p>As a matter of fact the point of the criterion reveals the patterns of The Basis of Deep
+                                Estimation</p>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <h3>Sed fringilla mauris sit</h3>
+                            <p>As a matter of fact the point of the criterion reveals the patterns of The Basis of Deep
+                                Estimation</p>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                    </div>
+                    <!-- /.row -->
+
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#services -->
+
+
+    <section id="one-of-kind">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="one-of-kind-inner">
+                        <h2 class="">One of a kind</h2>
+                        <h3>Quality digital Services<br>
+                            you Realy want!</h3>
+                    </div>
+                    <!-- /.one-of-kind-inner -->
+
+
+
+                    <div class="row">
+                        <div class="col-4">
+                            <!--  bootstap icon -->
+                            <i class="bi bi-upc-scan h1"></i>
+                            <h4>Tell us about your project</h4>
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente dicta ad beatae
+                                nobis iusto?</p>
+
+
+                        </div>
+                        <!-- /.col-4 -->
+                        <div class="col-4">
+                            <i class="bi bi-tree-fill h1"></i>
+                            <h4>
+                                We will make it happen
+                            </h4>
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente dicta ad beatae
+                                nobis iusto?</p>
+
+                        </div>
+                        <!-- /.col-4 -->
+                        <div class="col-4">
+                            <i class="bi bi-hypnotize h1"></i>
+                            <h4>
+                                We launch it & We support it</h4>
+
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente dicta ad beatae
+                                nobis iusto?</p>
+
+                        </div>
+                        <!-- /.col-4 -->
+
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.col-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#one-of-kind -->
+
+    <section id="ready">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="mb-4">Ready to get started?</h2>
+                    <a href="#" class="btn btn-lg btn-light">Get Started</a>
+                </div>
+                <!-- /.col-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#ready -->
+
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Contact Us</h2>
+                    <h3>Get in touch with us</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>
+                        Sunt sapiente dicta ad beatae nobis iusto?
+                    </p>
+                    <!-- bootstrap form contact  / firstname lastname email object meessage send message -->
+
+                </div>
+                <!-- /.col-12 -->
+                <div class="col-lg-6">
+                    <form action="">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="firstname">First Name</label>
+                                    <input type="text" class="form-control" id="firstname" placeholder="First Name">
+                                </div>
+                                <!-- /.form-group -->
+                            </div>
+                            <!-- /.col-6 -->
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="lastname">Last Name</label>
+                                    <input type="text" class="form-control" id="lastname" placeholder="Last Name">
+                                </div>
+                                <!-- /.form-group -->
+                            </div>
+                            <!-- /.col-6 -->
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" class="form-control" id="email" placeholder="Email">
+                                </div>
+                                <!-- /.form-group -->
+                            </div>
+                            <!-- /.col-6 -->
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="object">Object</label>
+                                    <input type="text" class="form-control" id="object" placeholder="Object">
+                                </div>
+                                <!-- /.form-group -->
+                            </div>
+                            <!-- /.col-6 -->
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="message">Message</label>
+                                    <textarea name="" id="message" cols="30" rows="5" class="form-control"
+                                        placeholder="Message"></textarea>
+                                </div>
+                                <!-- /.form-group -->
+                            </div>
+                            <!-- /.col-12 -->
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Send Message</button>
+                            </div>
+                            <!-- /.col-12 -->
+                        </div>
+                        <!-- /.row -->
+                    </form>
+                </div>
+                <!-- /.col-lg-6 -->
+                <div class="col-lg-6"></div>
+                <!-- /.col-lg-6 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#contact -->
+
+    <section id="get-in-touch" class="bg">
+        <h2>
+            Get in touch with us for any kind of digital services
+        </h2>
+    </section>
+    <!-- /#get-in-touch -->
+
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h2>Company</h2>
+                    <ul>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Our Team</a></li>
+                        <li><a href="#">Our Services</a></li>
+                        <li><a href="#">Our Clients</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <h2>Services</h2>
+                    <ul>
+                        <li><a href="#">Web Design</a></li>
+                        <li><a href="#">Web Development</a></li>
+                        <li><a href="#">Mobile App Development</a></li>
+                        <li><a href="#">Digital Marketing</a></li>
+                        <li><a href="#">SEO</a></li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <h2>Follow us</h2>
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">Linkedin</a></li>
+                        <li><a href="#">Youtube</a></li>
+                    </ul>
+                </div>
+                <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p>
+                        &copy; 2019 All Rights Reserved. Designed by <a href="#">BootstrapMade</a>
+                    </p>
+                </div>
+                <!-- /.col-12 -->
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /#footer -->
+</body>
+
+</html>
